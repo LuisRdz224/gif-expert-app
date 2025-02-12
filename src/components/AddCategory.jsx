e0fd469255956b5738f0c4635fd3319973c720b3
LuisRdz224 <luis.rodriguez@fyware.com>
2025-02-11 21:28:58 -0600

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export const AddCategory = ({ onNewCategory }) => {
+    const [inputValue, setinputValue] = useState('')
+
+    const onInputChange = ({ target }) => {
+        setinputValue(target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if (inputValue.trim().length <= 1) return
+        onNewCategory(inputValue.trim())
+        setinputValue('')
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder="Buscar gifs"
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+    )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
+}
